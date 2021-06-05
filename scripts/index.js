14f7be7a3fccda82078c605431b7b76770b8cc02
Adrian <adrian.neagu7@yahoo.com>
2021-06-06 00:11:34 +0300

    updated index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,28 @@
 //global variables 
-var fetchApi = new FetchApi();
-var productsList = [];
+let fetchApi = new FetchApi();
+let productsList = [];
 
+//HTML variables
+const mainContainer = document.querySelector("main");
 
 //Event Listener to get all products from server
 window.addEventListener("load", getAndDisplayProducts);
 
 function getAndDisplayProducts() {
-  fetchApi.getAllProducts()
-  .then(games => {
-    productsList = games;
-    productsList.forEach(game => addProductsToDom(game));
-  })
-}
+    fetchApi.getAllProducts()
+      .then(products => {
+        productsList = products;
+        productsList.forEach(product => addProductsToDom(product));
+      });
+  };
+function addProductsToDom(product) {
+  console.log(product);
+  let container = document.createElement('div');
+  container.setAttribute("id", product.id);
+  container.setAttribute("class", "col col-lg-3 col-md-4 col-sm-6");
+  // container.innerHTML = ``
 
-function addProductsToDom(game) {
-  
+
+  mainContainer.appendChild(container);
+
 }
