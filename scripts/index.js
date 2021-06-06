d9fe3228b4be80a8dada49b13b920b2da9ec0372
Adrian <adrian.neagu7@yahoo.com>
2021-06-06 20:42:22 +0300

    added fontAwesome script to html pages, updated pages

--- scripts/index.js
+++ scripts/index.js
@@ -19,12 +19,13 @@ function addProductsToDom(product) {
   console.log(product);
   let container = document.createElement('div');
   container.setAttribute("id", product.id);
-  container.setAttribute("class", "text-center mt-2 col col-lg-3 col-md-4 col-sm-6");
-  container.innerHTML = `<img class=" img-fluid" src="${product.imageUrl}">
+  container.setAttribute("class", "mh-25 text-center mt-2 col col-lg-3 col-md-4 col-sm-6");
+  container.innerHTML = `<img class=" img-fluid mh-50" src="${product.imageUrl}">
   <h2>${product.name}</h2>
-  <p>${product.price}</p> `;
+  <p><i class="fas fa-dollar-sign"></i> ${product.price}</p> `;
   let detailsButton = document.createElement("button");
   detailsButton.innerText = "Details";
+  detailsButton.setAttribute("class", "btn btn-primary");
   container.appendChild(detailsButton);
   detailsButton.addEventListener("click", (e) => {
     e.preventDefault();
