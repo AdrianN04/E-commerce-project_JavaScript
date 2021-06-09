177e57b44994057fa1af0ab5fee75bd52f98dc19
Adrian <adrian.neagu7@yahoo.com>
2021-06-09 22:24:45 +0300

    Stored added quantity to localStorage in detailsPage.js and made functionality for fetch in shoppingCart.js

--- scripts/detailsPage.js
+++ scripts/detailsPage.js
@@ -7,7 +7,8 @@ let idRecieved = urlParam.get('id');
 // HTML variables
 const detailsContainer = document.getElementById('detailsContainer');
 let productInput = document.querySelector("input");
-
+let alert = document.getElementById("alert");
+let message = document.getElementById("message");
 //fetch
 let fetchProductApi = new FetchApi();
 
@@ -24,7 +25,7 @@ function getResourcesFromPage() {
 }
 
 function displayProductDetails(product) {
-  
+
   product.id = idRecieved;
   console.log(product);
   document.getElementById("imageContainer").innerHTML = `<img
@@ -32,41 +33,52 @@ function displayProductDetails(product) {
   src="${product.imageUrl}"/>`;
   document.getElementById("productName").innerText = product.name;
   document.getElementById("productDescription").innerText = product.description;
-  document.getElementById("productPrice").innerHTML =`<i class="fas fa-dollar-sign"></i>  ${product.price}`;
-  document.getElementById("productQuantity").innerText ="Products in stoc: " + product.quantity;
+  document.getElementById("productPrice").innerHTML = `<i class="fas fa-dollar-sign"></i>  ${product.price}`;
+  document.getElementById("productQuantity").innerText = "Products in stoc: " + product.quantity;
 
   document.getElementById("cartButtonContainer").addEventListener("click", (e) => {
     e.preventDefault();
-      let input = checkInput(product);
-      if(input !== undefined) {
-        console.log(input)
-        setProductsToLocalStorage(product.id, input);
-      }
+    let input = checkInput(product);
+    if (input !== undefined) {
+      console.log(input)
+      setProductsToLocalStorage(product.id, input, product);
+    }
   });
 }
 
-function setProductsToLocalStorage(id, qty) {
-  localStorage.setItem(id, qty);
+function setProductsToLocalStorage(id, qty, product) {
+  let storedQuantity = localStorage.getItem(id);
+  if (storedQuantity !== null) {
+    let newQty = parseInt(storedQuantity) + qty;
+    console.log(newQty)
+    if (newQty <= parseInt(product.quantity)) {
+      localStorage.setItem(id, newQty);
+      showAction(message, `${newQty} x ${product.name} added to your shopping cart &#128513;`, true);
+    } else {
+      showAction(message, "The quantity you want is bigger than the actual stock! please check your cart!", true);
+    };
+
+  } else {
+    localStorage.setItem(id, qty);
+  }
+
 }
 
 function checkInput(product) {
-  let alert =  document.getElementById("alert");
-  let message = document.getElementById("message");
  
-    let valueFromInput = parseInt(productInput.value);
-    if(valueFromInput < 1 || productInput.value === "") {
-      showAction(alert, "Please insert a number!", false);
-      message.classList.add("hidden");
-    } 
-    else if(valueFromInput > parseInt(product.quantity)) {
-      showAction(alert, "Over stock limit!", false);
-      message.classList.add("hidden");
-    } 
-    else {
-      showAction(message, `${valueFromInput} x ${product.name} added to your shopping cart &#128513;`, true);
-      return valueFromInput;
-    };
-    
+
+  let valueFromInput = parseInt(productInput.value);
+  if (valueFromInput < 1 || productInput.value === "") {
+    showAction(alert, "Please insert a number!", false);
+    message.classList.add("hidden");
+  } else if (valueFromInput > parseInt(product.quantity)) {
+    showAction(alert, "Over stock limit!", false);
+    message.classList.add("hidden");
+  } else {
+    showAction(message, `${valueFromInput} x ${product.name} added to your shopping cart &#128513;`, true);
+    return valueFromInput;
+  };
+
 };
 
 
--- scripts/shoppingCart.js
+++ scripts/shoppingCart.js
@@ -1,29 +1,50 @@
 // import { FetchApi } from "./fetchApi";
 
-// let storageId = window.localStorage.getItem(storageId);
-// let storageId = window.localStorage.key()
-// console.log(storageId);
-window.addEventListener("load", retrieveKeysFromStorage);
-let cartList = [];
-let cartFetchApi = new FetchApi();
 
+window.addEventListener("load", retrieveKeysFromStorage);
+window.addEventListener("load", getAndDisplayCartProducts);
+
+let cartFetchApi = new FetchApi();
+let cartList = [];
 let keyList = [];
+let indexNumber =0;
+
 function retrieveKeysFromStorage() {
   for(let i=0; i < localStorage.length; i++) {
-    let keyId = window.localStorage.key(i);
-    console.log(keyId);
-    keyList.push(keyId);
+    let keyId =  localStorage.key(i);
+    keyList.push(keyId)
   }
-  
 }
 
 
 function getAndDisplayCartProducts() {
-  cartFetchApi.getAllProducts()
-      .then(products => {
-        console.log(products)
-        
-        // cartList = products;
-        // productsList.forEach(product => addProductsToDom(product));
-      });
+    for(let i=0; i < keyList.length; i++) {      
+      let keyId = keyList[i];   
+      if(keyId !== undefined){
+        cartFetchApi.getProduct(keyId)
+        .then(response => {
+          response.id = keyId;
+          addProductsToCart(response)
+        });
+      };
+    };
   };
+
+  function addProductsToCart(product) {
+    console.log(product)
+    indexNumber++;
+    let tableBody = document.getElementById("cartTableBody");
+    
+  
+    let tableRow = document.createElement("tr");
+    tableRow.setAttribute("id", product.id);
+    tableRow.setAttribute("class", "mt-3 align-middle container-fluid");
+    tableBody.appendChild(tableRow);
+
+    let tableIndex = document.createElement("th");
+    tableIndex.setAttribute("scope", "row");
+    tableIndex.setAttribute("class", "text-center");
+    tableIndex.innerText = indexNumber;
+    tableRow.appendChild(tableIndex);
+  }
+
